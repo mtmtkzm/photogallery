4568023557a694cede15141976f80e7b5cdf3e49
松本 和馬 <hello@mtmtkzm.com>
2016-12-03 01:55:42 +0900

    [add] filtering exif infomation

--- js/app.js
+++ js/app.js
@@ -30,19 +30,11 @@ getImage(0);
 /* *****
 フィルタリング */
 
+// フォーム
+var form = document.forms.filteringForm;
+
 // フィルター実行ボタン
 var button = document.getElementById('js-filter-button');
-
-var fmin = document.getElementById('filter-f-min');
-var fmax = document.getElementById('filter-f-max');
-var focalmin = document.getElementById('filter-focal-min');
-var focalmax = document.getElementById('filter-focal-max');
-var isomin = document.getElementById('filter-iso-min');
-var isomax = document.getElementById('filter-iso-max');
-var shuttermin = document.getElementById('filter-shutter-min');
-var shuttermax = document.getElementById('filter-shutter-max');
-var flashmin = document.getElementById('filter-flash-min');
-var flashmax = document.getElementById('filter-flash-max');
 
 // フィルタリングの値が入る
 var filteredExif = {};
@@ -50,23 +42,22 @@ var filteredExif = {};
 button.addEventListener('click', function (event) {
   filteredExif ={
     'f': {
-      min: '100',
-      max: '120'
+      min: form.fMin.value,
+      max: form.fMax.value
     },
-    'focal':  {
-      min: '80',
-      max: '100' 
+    'focal': {
+      min: form.focalMin.value,
+      max: form.focalMax.value
     },
     'iso': {
-      min: '400',
-      max: '1600'
+      min: form.isoMin.value,
+      max: form.isoMax.value
     },
     'shutter': {
-      min: '1/160',
-      max: '1/100'
+      min: form.shutterMin.value,
+      max: form.shutterMax.value
     },
-    'flash': 'true'
+    'flash': true
   };
   console.log(filteredExif);
 });
-
