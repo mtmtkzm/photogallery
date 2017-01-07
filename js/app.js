7b308c556672152fc01b0d3247046a1175de544a
松本 和馬 <hello@mtmtkzm.com>
2017-01-07 16:02:54 +0900

    update  default value

--- js/app.js
+++ js/app.js
@@ -2,18 +2,33 @@
 連番の画像（./images/oo.jpg）に対しHEADリクエスト。statusで画像数をカウント。 */
 
 var current = 0; // 最終的な返り値
-var imageArray = [];
+var allImages = []; // 写真一覧
+
 
 function getImage(num) {
-  var target = './images/img' + (num+1) + '.jpg';
+  var imgPath = './images/img' + (num+1) + '.jpg';
   var xmlhttp = new XMLHttpRequest();
 
-  xmlhttp.open('HEAD', target);
+
+
+  xmlhttp.open('HEAD', imgPath);
   xmlhttp.send();
   xmlhttp.addEventListener('readystatechange', function() {
     if (xmlhttp.readyState == 4) {
       if (xmlhttp.status == 200) {
-        imageArray.unshift(target);
+        var imgObj = {}; // imgのpathとexifのobj
+        imgObj = {
+          "path": imgPath,
+          "exif": {
+            "camera": "--",
+            "f": "--",
+            "focal": "--",
+            "iso": "--",
+            "shutter": "--",
+            "flash": "--"
+          }
+        }
+        allImages.unshift(imgObj);
         current += 1;
         getImage(num+1);
       } else {
@@ -26,38 +41,3 @@ function getImage(num) {
 
 getImage(0);
 
-
-/* *****
-フィルタリング */
-
-// // フォーム
-// var form = document.forms.filteringForm;
-
-// // フィルター実行ボタン
-// var button = document.getElementById('js-filter-button');
-
-// // フィルタリングの値が入る
-// var filteredExif = {};
-
-// button.addEventListener('click', function (event) {
-//   filteredExif ={
-//     'f': {
-//       min: form.fMin.value,
-//       max: form.fMax.value
-//     },
-//     'focal': {
-//       min: form.focalMin.value,
-//       max: form.focalMax.value
-//     },
-//     'iso': {
-//       min: form.isoMin.value,
-//       max: form.isoMax.value
-//     },
-//     'shutter': {
-//       min: form.shutterMin.value,
-//       max: form.shutterMax.value
-//     },
-//     'flash': true
-//   };
-//   console.log(filteredExif);
-// });
